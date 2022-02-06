bbe5dce08b6fae29be1ba0636373850aed1e25ba
Eric Fithian <ef2413@columbia.edu>
2022-02-06 11:03:38 -0500

    Updated with the real db

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), # <- here we have added the new path
     path('about/', views.About.as_view(), name="about"),
-    path('artists/', views.ArtistList.as_view(), name="artists")
+    path('artists/', views.ArtistList.as_view(), name="artist_list")
 ]
